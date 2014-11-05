494ab3961b2e9379e560218f5ed662609314c580
joey0214 <joey0576@163.com>
2014-11-05 10:59:49 -0500

    20141105shell

--- katak/5genomeAlignment.sh
+++ katak/5genomeAlignment.sh
@@ -1,19 +1,27 @@
 #!/user/bin/bash
 
+# Job: 5genomes alignment
+# Submit date: 2014-11-05
+# Program: muscle
+
 #$ -N TEST_BLAST01
 ##The name of the job. Files created by SGE for standard error and standard output take this name
+$ -N ALIGNMENT_5GENOMES_20141105
 
 #$ -M your_email_here
 ##Your email if you want to receive notification
+$ -m joey0214.zhong@gmail.com
 
 #$ -m bea
 ##Types of notification
 ##		b: when job start
 ##		e: when job finish
 ##		a: when job is stopped or killed by the system or the user
+$-m bea
 
 #$ -pe smp 2
 ##Number of cpu/core
+$ -pe smp 6
 
 #$ -l h_vmem=8G
 ##Resource limit parameters, "virtual memory" needed for the computation
@@ -29,13 +37,12 @@
 
 
 #Some environmental variables here
-export PRG=/prg/ncbiBlast/current/bin
-export BANK=/biodata/blastdb
+# export PRG=/prg/ncbiBlast/current/bin
+# export BANK=/biodata/blastdb
 
 #Write the command line of your analyse here
-$PRG/blastp -db $BANK/nr -query 1433_pea.fasta -evalue 1e-5 -num_threads 2 -out 
-1433_pea_vs_nr.blastp
+
+$./muscle3.8.31_i86linux64 -in ./alignment/5genomes/5yeast_genomes.fasta -out ./alignment/5genomes/alignment_output.fasta -maxiters 1 -diags
 
 
 ##then run "qsub SGE_example.sh" to submit job
-## to delete/stop a job: "qdel job_id"
